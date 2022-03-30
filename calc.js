691954a1ab49b2aec4550b1da0816b3cebafd3c0
Rlaur2 <plaureanopena@gmail.com>
2022-03-30 14:54:07 -0400

    Complete basic functionality

--- calc.js
+++ calc.js
@@ -1,3 +1,4 @@
+//the four basic functions for the operators
 const add = (a,b) => {
     return a + b;
 };
@@ -28,6 +29,7 @@ const divide = (a,b) => {
     return a / b;
 };
 
+//the function that evaluates the expression and returns the answer
 const operate = (number1, operator, number2) => {
     if (operator === '+') {
         return add(number1,number2);
@@ -40,6 +42,7 @@ const operate = (number1, operator, number2) => {
     }
 }
 
+//DOM manipulation variables
 const numberButtons = document.querySelectorAll('.number');
 const mainDisplay = document.querySelector('.main-display');
 const miniDisplay = document.querySelector('.mini-display');
@@ -56,15 +59,19 @@ const equalButton = document.querySelector('#equal');
 //function to display clicked numbers and save to variable
 let operand = '';
 function displayNumber(e) {
+    //'operand' class distinguishes user-inputted numbers from numbers that were evaluated results
+    //after inputting a single number, operand class is removed
     if (mainDisplay.className.includes('operand')) {
         mainDisplay.textContent = '';
         mainDisplay.classList.remove('operand');
     }
-    if (mainDisplay.textContent.length > 8) {
+    //code to disable numbers when inputted length is too long
+    if (mainDisplay.textContent.length > 12) {
         for (numberButton of numberButtons) {
             numberButton.removeEventListener('click',displayNumber);
         }
     }
+    //keypad's text is used fill out the main display
     mainDisplay.textContent += this.textContent;
     operand = mainDisplay.textContent;
 }
@@ -74,14 +81,17 @@ for (numberButton of numberButtons){
     numberButton.addEventListener('click',displayNumber);
 }
 
+//code that controls the four operators
 let operatorPlug = ''
 let equation = [];
-
 function operation(e) {
-    //beginnings of codeto fix clicking on operator multiple times 
-    /*if (equation.length === 1) {
-
-    }*/
+    //if statement to catch unique situations when clicking on operators with an operator already chosen 
+    if (equation.length === 1 && Number(operand) === equation[0] && !mainDisplay.className.includes('operand')) {
+        solution();
+    } //else if to suppliment the above 
+    else if (equation.length === 1 && operand != equation[0]) {
+        solution();
+    } //code based of clicked button to decide which operator function to be used 
     if (this.id === 'division') {
         operatorPlug = '/';       
     } if (this.id === 'multiplication') {
@@ -90,24 +100,27 @@ function operation(e) {
         operatorPlug = '-';
     } if (this.id === 'addition') {
         operatorPlug = '+';
+    } //operators only push when array is empty 
+    if (equation.length === 0) {
+        equation.push(Number(operand));
     }
-    equation.push(Number(operand));
+    //code to indicate number in display is not user-inputted anymore 
     mainDisplay.classList.add('operand');
+    //Code to re-enable keypad in case user inputted max length number
     for (numberButton of numberButtons) {
         numberButton.addEventListener('click',displayNumber);
     }
 };
 
+//Code that uses current number in display to evaluate along with previously entered number
+//also clears and resets certain things
 let answer = ''
 const solution = () => {
-   //code meant to fix situation of hitting enter without enough operands
-    /* if (equation.length < 1) {
-        return;
-    } */
     equation.push(Number(operand));
     answer = operate(equation[0],operatorPlug,equation[1]);
     equation = [];
-    equation.push(answer);
+    operatorPlug = '';
+    operand = answer;
     mainDisplay.textContent = answer;
     mainDisplay.classList.add('operand');
     for (numberButton of numberButtons) {
@@ -115,9 +128,21 @@ const solution = () => {
     }
 }
 
+//function to clear out the calculator of all info
+const reset = () => {
+    equation = [];
+    mainDisplay.textContent = '';
+    operatorPlug = '';
+    operand = '';
+    mainDisplay.classList.remove('operand');
+    for (numberButton of numberButtons){
+        numberButton.addEventListener('click',displayNumber);
+    };
+};
 
 divideButton.addEventListener('click',operation);
 multiplyButton.addEventListener('click',operation);
 minusButton.addEventListener('click',operation);
 additionButton.addEventListener('click',operation);
 equalButton.addEventListener('click',solution);
+clearButton.addEventListener('click',reset);
